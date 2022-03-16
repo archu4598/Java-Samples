a9d113c88f2c5d1091fd0783dc5b0ffe5fe2450d
archu4598 <101569942+archu4598@users.noreply.github.com>
2022-03-16 13:39:26 +0530

    Update2

--- src/packagename/EmpSalaryOperation.java
+++ src/packagename/EmpSalaryOperation.java
@@ -5,7 +5,7 @@ public class EmpSalaryOperation {
 	public static void main(String[] args) {
 		Employee e = new Employee("Archana", "Software Engineer");
 		Empsalarydetails emp1 = new Empsalarydetails(new Employee("Archana", "Software Engineer"), "Aswanth", 123);
-
+                System.out.println("Hello");
 	}
 
 }
