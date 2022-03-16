56eaac8473ad6e4ca6a293a6a57e3a5bba7e04ad
archu4598 <archanasrinivasanks@gmail.com>
2022-03-16 13:31:26 +0530

    Second commit

--- src/packagename/constructorover.java
+++ src/packagename/constructorover.java
@@ -5,7 +5,7 @@ public class constructorover {
 	public static void main(String[] args) {
 	     Trycatch tc = new Trycatch(4, "Sd");
 	     Trycatch tc1 = new Trycatch(5, "ds");
-
+           System.out.println("Hi");
 	     tc.example1();
 	}
 
